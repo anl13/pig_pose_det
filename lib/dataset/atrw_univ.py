3afb71aad1ef85931b20e1bd6b665f29e31de8c1
anl13 <anl13@mail.tsinghua.org.cn>
2019-12-21 09:11:23 +0800

    fix eval bug

--- lib/dataset/atrw_univ.py
+++ lib/dataset/atrw_univ.py
@@ -84,14 +84,9 @@ class ATRWUnivDataset(JointsDataset):
         self.parent_ids = None
         self.upper_body_ids = (0,1,2,3,4,5,6,7,8,9,10,17,19)
         self.lower_body_ids = (11,12,13,14,15,16,18,21,22,20)
-
-        # self.joints_weight = np.array(
-        #     [
-        #         1.2, 1., 1., 1.2, 1., 1.2, 1.,
-        #         2.0, 1.5, 1.2, 1.2, 1.1, 1.1, 1., 1.1
-        #     ],
-        #     dtype=np.float32
-        # ).reshape((self.num_joints, 1))                   # data: 0716
+        self.sigmas = np.asarray([
+                25,25,25,35,35, 79,79,72,72,62,62,79,79,72,72,62,62,100,25,100,100,50,30
+            ])/2000.0
         self.joints_weight = np.array(
             [
                 1,1,1,1,1,
@@ -340,12 +335,12 @@ class ATRWUnivDataset(JointsDataset):
             if self.soft_nms:
                 keep = soft_oks_nms(
                     [img_kpts[i] for i in range(len(img_kpts))],
-                    oks_thre
+                    oks_thre, sigmas=self.sigmas
                 )
             else:
                 keep = oks_nms(
                     [img_kpts[i] for i in range(len(img_kpts))],
-                    oks_thre
+                    oks_thre, sigmas=self.sigmas
                 )
 
             if len(keep) == 0:
